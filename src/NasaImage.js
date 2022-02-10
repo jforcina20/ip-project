86ec77a7d5d728373f3292b7d6d6143e462634ae
John Forcina <jforcina20@gmail.com>
2022-02-10 12:54:48 -0500

    Combined options

--- src/NasaImage.js
+++ src/NasaImage.js
@@ -1,110 +1,114 @@
 import { LitElement, html, css } from 'lit';
 import '@lrnwebcomponents/accent-card/accent-card.js';
 
-export class NasaImage extends LitElement {
-    static get tag() {
-      return 'nasa-image-search';
-    }
+class NasaImageSearch extends LitElement {
+  constructor() {
+    super();
+    this.images = [];
+    this.loadData = false;
+    this.name = 'moon';
+    this.page = 1;
+    this.listOnly = false;
+    this.year_start = 2000;
+    this.year_end = 2021;
+  }
 
-    constructor() {
-        super();
+  static get properties() {
+    return {
+      images: { type: Array },
+      loadData: {
+        type: Boolean,
+        reflect: true,
+        attribute: 'load-data',
+      },
+      name: { type: String },
+      page: { type: Number },
+      listOnly: {
+        type: Boolean,
+        reflect: true,
+        attribute: 'list-only',
+      },
+      year_start: { type: Number },
+      year_end: { type: Number },
+    };
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (propName === 'name' && this[propName]) {
+        this.getData();
+      } else if (propName === 'page' && this[propName]) {
+        this.getData();
+      }
+    });
+  }
+
+  async getData() {
+    return fetch(
+      `https://images-api.nasa.gov/search?q=${this.name}&media_type=image&page=${this.page}&year_start=${this.year_start}&year_end=${this.year_end}`
+    )
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        return false;
+      })
+      .then(data => {
         this.images = [];
-        this.searchImages = true;
-        this.listOnly = false;
-        this.search = "Moon Landing";
-    }
+        for (let i = 0; i < data.collection.items.length; i++) {
+          const element = {
+            image: data.collection.items[i].links[0].href,
+            descriptions: data.collection.items[i].data[0].description,
+            heading: data.collection.items[i].data[0].title,
+            author: data.collection.items[i].data[0].secondary_creator,
+          };
+          this.images.push(element);
+        }
+        // tell the browser to wait for 1 second
+        setTimeout(() => {
+          this.loadData = false;
+        }, 1000);
+      });
+  }
 
-    static get properties() {
-        return {
-            images: {
-                type: Array,
-            },
-            searchImages: {
-                type: Boolean,
-                reflect: true,
-                attribute: 'search-images',
-            },
-            listOnly: {
-              type: Boolean,
-              reflect: true,
-              attribute: 'list-only',
-            },
-            search: {type: String, reflect: true},
-            }
-        };
-    
-        updated(changedProperties) {
-            changedProperties.forEach((oldValue, propName) => {
-              if (propName === 'searchImages' && this[propName]) {
-                this.getImages();
-              }
-              else if (propName === 'search') {
-                this.dispatchEvent(
-                  new CustomEvent('results-changed', {
-                    detail: {
-                      value: this.search,
-                    },
-                  })
-                );
-              }
-            });
-          }
-        
+  static get styles() {
+    return css`
+      :host([view='list']) ul {
+        margin: 20px;
+      }
+    `;
+  }
 
-    async getImages() {
-        console.log(this.search);
-        return fetch('https://images-api.nasa.gov/search?media_type=image&q=' + this.search)
-        .then((response) => {
-            if (response.ok) return response.json();
-        })
-        .then((data) => {
-            this.images = [];
-            var i = 0;
-            while(i < data.collection.items.length) {
-                const resultInfo = {
-                    image: data.collection.items[i].links[0].href,
-                    title: data.collection.items[i].data[0].title,
-                    description: data.collection.items[i].data[0].description,
-                    author: data.collection.items[i].data[0].secondary_creator,
-                };
-                i++;
-                console.log(resultInfo);
-                this.images.push(resultInfo);
-            }
-
-            setTimeout(() => {
-                this.searchImages = false;
-              }, 1000);
-            
-        });
-    }
-
-    render() {
-      return html`
-      ${this.listOnly === true
-        ? html`
-            <ul>
-              ${this.images.map(
-                item => html`
-                  <li>
-                    <b>Image URL: </b>${item.image} - <b>Title: </b>${item.title} - <b>Description: </b>${item.description} - <b>Credit:</b>${item.author}
-                  </li>
-                `
-              )}
-            </ul>
-          `
-        : html`
+  render() {
+    return html` ${this.listOnly === true
+      ? html`
+          <ul>
             ${this.images.map(
-                item => html`
-                    <accent-card image-src="${item.image}">
-                        <div slot="heading">${item.title}</div>
-                        <div slot="content">${item.description}</div>
-                        <div slot="content">Credit: ${item.author}</div>
-                    </accent-card>`
-            )}`
-    }`
-
+              item => html`
+                <li>
+                  <a href="${item.image}" target="_blank"><b>Image Source</b></a>-
+                  <b>Heading: </b>${item.heading} -
+                  <b>Description: </b>${item.descriptions} -
+                  <b>Author: </b>${item.author}
+                </li>
+              `
+            )}
+          </ul>
+        `
+      : html` ${this.images.map(
+          item => html`
+          <accent-card
+            image-src="${item.image}"
+            accent-color="blue"
+            accent-heading
+            style="max-width:2000px;"
+          >
+            <div slot="heading"><a href="${item.image}" target="_blank">${item.heading}</a></div>
+            <div slot="content"><b>Description: </b>${item.descriptions}</div>
+            <div slot="content"><b>Credit: </b>${item.author}</div>
+          ></accent-card>`
+        )}`}`;
   }
 }
 
-customElements.define(NasaImage.tag, NasaImage);
+customElements.define('nasa-image-search', NasaImageSearch);
